fce69711cb2b4903e142967c08881f166b32f746
swiftaff <swiftaff@gmail.com>
2020-03-08 22:28:31 +0800

    getting canvas working

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,27 +1,32 @@
-use std::f64;
+//use std::f64;
 
-mod tuples;
+use crate::tuples;
 
-#[derive(Debug)]
-pub struct Pixel_canvas {
-    pub data: [tuples::Color],
+#[derive(Debug, Clone)]
+pub struct PixelCanvas {
     pub width: u32,
-    pub height: u32
+    pub height: u32,
+    pub length: u32,
+    pub data: Vec<tuples::Color>
 }
 
-pub fn pixel_canvas(width:u32,height:u32, default_color:Color) -> Pixel_canvas {
-    let data [u32; width * height] = [default_color; width * height];
+pub fn pixel_canvas(width:u32,height:u32, default_color:tuples::Color) -> PixelCanvas {
     let length = width * height;
-    { data:data, width:width, height:height, length:length }
+    let mut data = Vec::with_capacity(length as usize);
+    for _i in 0..length {
+        data.push(default_color);
+    }
+    PixelCanvas { data:data, width:width, height:height, length:length }
 }
 
-pub fn pixel_write(canvas: tuples::Pixel_canvas, x: u32, y: u32, col: tuples::Color) -> Pixel_canvas {
-    let index = (c.width * y + x) as u32;
-    canvas.data[index] = col;
-    canvas
+pub fn pixel_write(canvas: PixelCanvas, x: u32, y: u32, col: tuples::Color) -> PixelCanvas {
+    let index = (canvas.width * y + x) as usize;
+    let mut new_canvas = canvas;
+    new_canvas.data[index] = col;
+    new_canvas
 }
-
-fn pixel_get(canvas: Pixel_canvas, x: u32, y: u32) -> tuples::Color {
+/*
+fn pixel_get(canvas: PixelCanvas, x: u32, y: u32) -> tuples::Color {
     let index = canvas.width * y + x;
     if index >= 0 && index < canvas.length {
         canvas.data[index];
@@ -29,7 +34,7 @@ fn pixel_get(canvas: Pixel_canvas, x: u32, y: u32) -> tuples::Color {
     canvas
 }
 
-pub fn ppm_get(c: Pixel_canvas) -> String {
+pub fn ppm_get(c: PixelCanvas) -> String {
     let w = c.width.to_string();
     let h = c.height.to_string();
     let limit = "255";
@@ -37,7 +42,7 @@ pub fn ppm_get(c: Pixel_canvas) -> String {
     "P3\n" + w + " " + h + "\n" + limit + "\n" + data
 }
 
-fn str_from_canvas_data_get(c: Pixel_canvas, clampLimit:u32) {
+fn str_from_canvas_data_get(c: PixelCanvas, clampLimit:u32) {
     let colorStringArray = [];
     let rowArray = [];
     c.data.map(col => colorStringArray.push(getString_fromColor(col, clampLimit)));
@@ -84,4 +89,34 @@ fn color_clamp(col:Color) {
     if col.green > 1 {col.green = 1;}
     if col.blue > 1 {col.blue = 1;}
     col
+}
+*/
+
+#[cfg(test)]
+mod tests {
+use super::*;
+
+    #[test]
+    fn test_pixel_canvas() {
+        //Creating a pixelCanvas
+        let c = tuples::color(1.0,2.0,3.0);
+        let pc =pixel_canvas(10,20,c);
+        assert_eq!(pc.width, 10);
+        assert_eq!(pc.height, 20);
+        assert_eq!(pc.length, 200);
+        assert_eq!(tuples::get_bool_colors_are_equal(&pc.data[11], &c),true)
+    }
+
+    #[test]
+    fn test_pixel_write() {
+        //Writing a pixel to pixelCanvas
+        let black = tuples::color(0.0,0.0,0.0);
+        let mut pc = pixel_canvas(10,20,black);
+        assert_eq!(tuples::get_bool_colors_are_equal(&pc.data[32], &black),true);
+
+        let red = tuples::color(1.0,0.0,0.0);
+        pc = pixel_write(pc,2,3,red);
+        assert_eq!(tuples::get_bool_colors_are_equal(&pc.data[32], &red),true)
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
 mod tuples;
 mod canvas;
@@ -7,7 +7,7 @@ fn main(){
     println!("fire canon");
     let black = tuples::color(0.0, 0.0, 0.0);
     let mut orange = tuples::color(1.0,1.0,0.0);
-    let c = canvas::pixel_canvas(500, 500, black);
+    let mut c = canvas::pixel_canvas(500, 500, black);
     // velocity is normalized to 1 unit/tick.
     let mut proj = tuples::projectile(tuples::point(0.0, 1.0, 0.0), tuples::vector(5.0, 10.0, 0.0));
 
@@ -18,12 +18,13 @@ fn main(){
     println!("Projectile Velocity {:?}", proj.velocity);
     println!("Environment Gravity {:?}", env.gravity);
     println!("Environment Wind {:?}", env.wind);
-    println!(" ");
+    println!(" {}", c.length);
 
     let start = Instant::now();
     while proj.position.y > 0.0 {
+        let y = &c.height - proj.position.y as u32;
         //println!("Tick: {:?}. Projectile Position ({:?},{:?}). Velocity:{:?}", tick_count, proj.position.x, proj.position.y, proj.velocity);
-        c = pixel_write(&c, proj.position.x as u32, c.height - proj.position.y as u32, orange);
+        c = canvas::pixel_write(c, proj.position.x as u32, y, orange);
         tick_count = tick_count + 1;
         orange.red = orange.red - 0.01;
         orange.green = orange.green - 0.01;
