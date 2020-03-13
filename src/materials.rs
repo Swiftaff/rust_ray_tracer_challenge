5f1fd9cd01005ac49106343ad3570aec819900ac
swiftaff <swiftaff@gmail.com>
2020-03-13 23:11:42 +0800

    materials done

--- src/materials.rs
+++ src/materials.rs
@@ -1,4 +1,13 @@
+use crate::lights;
 use crate::tuples;
+
+pub const MATERIAL_DEFAULT: Material = Material {
+    color: tuples::COLOR_WHITE,
+    ambient: 0.1,
+    diffuse: 0.9,
+    specular: 0.9,
+    shininess: 200.0,
+};
 
 #[derive(Debug, Copy, Clone)]
 pub struct Material {
@@ -25,14 +34,6 @@ pub fn material(
     }
 }
 
-pub const MATERIAL_DEFAULT: Material = Material {
-    color: tuples::COLOR_BLACK,
-    ambient: 0.1,
-    diffuse: 0.9,
-    specular: 0.9,
-    shininess: 200.0,
-};
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -49,5 +50,71 @@ mod tests {
         assert_eq!(a.diffuse, 0.9);
         assert_eq!(a.specular, 0.9);
         assert_eq!(a.shininess, 200.0);
+    }
+
+    #[test]
+    fn test_lighting_surface_eye_light() {
+        //Lighting with the eye between the light and the surface
+        let eyev = tuples::vector(0.0, 0.0, -1.0);
+        let normalv = tuples::vector(0.0, 0.0, -1.0);
+        let light = lights::light_point(tuples::point(0.0, 0.0, -10.0), tuples::COLOR_WHITE);
+        let result = lights::lighting(MATERIAL_DEFAULT, light, tuples::POINT_ORIGIN, eyev, normalv);
+        assert_eq!(
+            tuples::get_bool_colors_are_equal(&result, &tuples::color(1.9, 1.9, 1.9)),
+            true
+        );
+    }
+
+    #[test]
+    fn test_lighting_surface_eye_light_eye_offset_45deg() {
+        //Lighting with the eye between light and surface, eye offset 45°
+        let eyev = tuples::vector(0.0, 2.0_f64.sqrt() / 2.0, -2.0_f64.sqrt() / 2.0);
+        let normalv = tuples::vector(0.0, 0.0, -1.0);
+        let light = lights::light_point(tuples::point(0.0, 0.0, -10.0), tuples::COLOR_WHITE);
+        let result = lights::lighting(MATERIAL_DEFAULT, light, tuples::POINT_ORIGIN, eyev, normalv);
+        assert_eq!(
+            tuples::get_bool_colors_are_equal(&result, &tuples::color(1.0, 1.0, 1.0)),
+            true
+        );
+    }
+
+    #[test]
+    fn test_lighting_eye_opposite_surface_light_offset_45deg() {
+        //Lighting with eye opposite surface, light offset 45°
+        let eyev = tuples::vector(0.0, 0.0, -1.0);
+        let normalv = tuples::vector(0.0, 0.0, -1.0);
+        let light = lights::light_point(tuples::point(0.0, 10.0, -10.0), tuples::COLOR_WHITE);
+        let result = lights::lighting(MATERIAL_DEFAULT, light, tuples::POINT_ORIGIN, eyev, normalv);
+        assert_eq!(
+            tuples::get_bool_colors_are_equal(&result, &tuples::color(0.7364, 0.7364, 0.7364)),
+            true
+        );
+    }
+
+    #[test]
+    fn test_lighting_eye_in_path_reflection_vector() {
+        //Lighting with eye in the path of the reflection vector
+        let eyev = tuples::vector(0.0, -2.0_f64.sqrt() / 2.0, -2.0_f64.sqrt() / 2.0);
+        let normalv = tuples::vector(0.0, 0.0, -1.0);
+        let light = lights::light_point(tuples::point(0.0, 10.0, -10.0), tuples::COLOR_WHITE);
+        let result = lights::lighting(MATERIAL_DEFAULT, light, tuples::POINT_ORIGIN, eyev, normalv);
+        assert_eq!(
+            tuples::get_bool_colors_are_equal(&result, &tuples::color(1.6364, 1.6364, 1.6364)),
+            true
+        );
+    }
+
+    #[test]
+    fn test_lighting_light_behind_surface() {
+        //Lighting with the light behind the surface
+        let eyev = tuples::vector(0.0, 0.0, -1.0);
+        let normalv = tuples::vector(0.0, 0.0, -1.0);
+        let light = lights::light_point(tuples::point(0.0, 0.0, 10.0), tuples::COLOR_WHITE);
+        let result = lights::lighting(MATERIAL_DEFAULT, light, tuples::POINT_ORIGIN, eyev, normalv);
+        println!("result {},{},{}", result.red, result.green, result.blue);
+        assert_eq!(
+            tuples::get_bool_colors_are_equal(&result, &tuples::color(0.1, 0.1, 0.1)),
+            true
+        );
     }
 }
